e85d528c40113082545b25f4e6da9631c7b6c9a7
akillll22 <mutawakkielr@gmail.com>
2025-06-20 23:13:58 +0700

    commit

--- lib/screens/history_page.dart
+++ lib/screens/history_page.dart
@@ -1,15 +1,66 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HistoryPage extends StatelessWidget {
+class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
 
   @override
+  State<HistoryPage> createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  List<Map<String, dynamic>> _history = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHistory();
+  }
+
+  Future<void> _loadHistory() async {
+    final prefs = await SharedPreferences.getInstance();
+    final data = prefs.getStringList('quiz_history') ?? [];
+
+    setState(() {
+      _history = data
+          .map((e) => json.decode(e) as Map<String, dynamic>)
+          .toList()
+          .reversed
+          .toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Riwayat kuis akan muncul di sini.',
-        style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riwayat Kuis'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
       ),
+      body: _history.isEmpty
+          ? const Center(child: Text('Belum ada kuis yang dikerjakan.'))
+          : ListView.builder(
+              itemCount: _history.length,
+              itemBuilder: (context, index) {
+                final item = _history[index];
+                final time = DateTime.parse(item['time']).toLocal();
+
+                return ListTile(
+                  title: Text(
+                    item['category']
+                        .toString()
+                        .replaceAll('_', ' ')
+                        .toUpperCase(),
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    '${item['score']} dari ${item['total']} soal\n${time.day}/${time.month}/${time.year} ${time.hour}:${time.minute}',
+                  ),
+                );
+              },
+            ),
     );
   }
 }
